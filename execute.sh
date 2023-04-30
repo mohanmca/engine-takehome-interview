fe41f49c22fbcf6ec2add69cb0dec4779542116a
mohanmca <mohanmca@gmail.com>
2023-04-30 14:38:45 +0800

    Docker file is updated

--- execute.sh
+++ execute.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mvn clean package
-java -jar target/engine-takehome-1.0-SNAPSHOT.jar < src/test/resources/sample_input.csv
+java -jar /app/target/engine-takehome-1.0-SNAPSHOT.jar < /app/sample_input.txt
